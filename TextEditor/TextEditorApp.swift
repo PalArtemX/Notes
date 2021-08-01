ee50323cd9cd5d108223dab66465cb8054ac9803
Artem Palyutin <pal.artem@icloud.com>
2021-08-01 23:32:35 +0300

    Initial Commit

---
+++ TextEditor/TextEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextEditorApp.swift
+//  TextEditor
+//
+//  Created by Artem Palyutin on 01.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct TextEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
